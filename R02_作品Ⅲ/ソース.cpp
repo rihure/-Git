90e781724c12314c3bf9598479afd0e97f382320
EISAN_ <hj-g19015@sist.ac.jp>
2020-12-07 11:45:56 +0900

    現在のソースコード

--- R02_作品Ⅲ/ソース.cpp
+++ R02_作品Ⅲ/ソース.cpp
@@ -1,19 +1,727 @@
 
 #include "DxLib.h"
 
-// プログラムは WinMain から始まります
+
+#define MAP_NUM         3           // マップの数
+#define GAME_WIDTH			800	//画面の横の大きさ
+#define GAME_HEIGHT			640	//画面の縦の大きさ
+#define GAME_COLOR			32	//画面のカラービット
+
+#define MAP_SIZE        32          // マップチップ一つのドットサイズ
+
+#define MAP_WIDTH_MAX   25          // マップの最大幅
+#define MAP_HEIGHT_MAX  20          // マップの最大縦長さ
+
+#define MAP_DIV_WIDTH		32	//画像を分割する幅サイズ
+#define MAP_DIV_HEIGHT		32	//画像を分割する高さサイズ
+#define MAP_DIV_TATE		8	//画像を縦に分割する数8
+#define MAP_DIV_YOKO		50	//画像を横に分割する数50
+#define MAP_DIV_NUM	MAP_DIV_TATE * MAP_DIV_YOKO	//画像を分割する総数 400
+
+#define GAME_FPS			60	//FPSの数値	
+
+//画像のパス設定
+#define TITLE_BACK_PATH TEXT(".\\IMAGE\\ダウンロード (2).png") //タイトルの画像
+#define IMAGE_SETUMEI_PATH		TEXT(".\\IMAGE\\操作説明2.png") //説明画面の画像
+#define GAME_MAP_PATH TEXT(".\\IMAGE\\ST-Town-I01.png")
+
+//エラーメッセージ
+#define IMAGE_LOAD_ERR_TITLE	TEXT("画像読み込みエラー")
+
+//パスの長さ
+#define PATH_MAX			255	//255文字まで
+#define NAME_MAX			255	//255文字まで
+
+//キーボードの種類
+#define KEY_CODE_KIND		256	//256種類
+
+//閉じるボタンを押したとき
+#define MSG_CLOSE_TITLE			TEXT("終了メッセージ")
+#define MSG_CLOSE_CAPTION		TEXT("ゲームを終了しますか？")
+
+enum GAME_SCENE {
+	GAME_SCENE_START,
+	GAME_SCENE_SETUMEI,
+	GAME_SCENE_PLAY,
+	GAME_SCENE_END,
+};	//ゲームのシーン
+
+enum GAME_MAP_KIND
+{
+	/*n = -1,*/	//(NONE)未定
+	k = 10,	//床
+	t = 17,	//上壁
+	s = 25,	//中壁
+	g = 33,	//下壁
+	n = 196, // 虚無
+	d = 151, // ダンボール
+	a = 67, //左窓上
+	b = 68, // 右窓上
+	c = 75, //左窓下
+	d = 76, // 右窓下
+	e = 350, //カーペット左
+	f = 351 //カーペット真ん中
+};	//マップの種類
+
+enum GAME_END {
+	GAME_END_COMP,	//コンプリート
+	GAME_END_FAIL	//フォールト
+};	//ゲームの終了状態
+
+
+
+
+typedef struct STRUCT_IMAGE
+{
+	char path[PATH_MAX];		//パス
+	int handle;					//ハンドル
+	int x;						//X位置
+	int y;						//Y位置
+	int width;					//幅
+	int height;					//高さ
+
+}IMAGE;	//画像構造体
+
+typedef struct STRUCT_MAP_IMAGE
+{
+	char path[PATH_MAX];				//パス
+	int handle[MAP_DIV_NUM];			//分割したの弾の画像ハンドルを取得
+	int kind[MAP_DIV_NUM];				//マップの種類
+	int width;							//幅
+	int height;							//高さ
+}MAPCHIP;	//MAP_IMAGE構造体
+
+typedef struct STRUCT_MAP
+{
+	GAME_MAP_KIND kind;			//マップの種類
+	int x;						//X位置
+	int y;						//Y位置
+	int width;					//幅
+	int height;					//高さ
+}MAP;	//MAP構造体
+
+//マップ
+GAME_MAP_KIND MapData[MAP_HEIGHT_MAX][MAP_WIDTH_MAX] =
+{
+	/*0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+	3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3*/
+
+	n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,
+	n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,
+	n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,n,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,
+	k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k
+};
+
+int MapData_Object[MAP_WIDTH_MAX][MAP_HEIGHT_MAX] =
+{	
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,67,68,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,75,76,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	151,151,151,151,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,350,351,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	151,151,151,151,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,151,151,151,151,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	151,151,151,151,0,0,0,0,0,0,0,0,0,151,151,151,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,151,151,151,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	151,151,151,151,0,0,0,0,0,0,0,0,151,151,151,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,151,0,0,0,0,151,151,0,0,0,0,
+	151,151,151,151,0,0,0,0,0,0,0,0,0,0,151,0,0,151,0,0,0,151,0,0,0,
+	0,0,0,0,0,0,151,151,151,151,0,0,0,0,0,0,0,0,0,0,0,151,0,0,0,
+	151,151,151,151,0,0,0,0,0,151,151,0,0,0,0,0,0,0,0,0,151,151,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,151,0,151,151,0,0,151,0,0,0,0,
+	151,151,151,151,0,0,0,0,0,0,0,0,0,151,151,0,0,151,0,151,151,0,0,0,0,
+	0,0,0,0,0,0,0,0,151,151,0,151,151,151,0,0,0,151,151,151,0,0,0,0,0,
+	151,151,151,151,0,0,0,0,0,151,151,151,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,151,151,151,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,151,0
+};
+
+GAME_MAP_KIND MapData_NoWalk[MAP_HEIGHT_MAX][MAP_WIDTH_MAX] =
+{
+	/*17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,
+	25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,25,
+	33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,33,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*/
+
+	t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,
+	s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,s,
+	g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,
+	
+};
+
+int mapDataInit[MAP_HEIGHT_MAX][MAP_WIDTH_MAX];
+int mapDataInit_Nowalk[MAP_HEIGHT_MAX][MAP_WIDTH_MAX];
+int mapDataInit_Object[MAP_HEIGHT_MAX][MAP_WIDTH_MAX];
+
+//マップチップの画像を管理
+MAPCHIP mapChip;
+MAPCHIP mapChip_Nowalk;
+MAPCHIP mapChip_Object;
+
+//マップの場所を管理
+MAP map[MAP_HEIGHT_MAX][MAP_WIDTH_MAX];
+int GameScene;		//ゲームシーンを管理
+//キーボードの入力を取得
+char AllKeyState[KEY_CODE_KIND] = { '\0' };		//すべてのキーの状態が入る
+char OldAllKeyState[KEY_CODE_KIND] = { '\0' };	//すべてのキーの状態(直前)が入る
+
+//FPS関連
+int StartTimeFps;				//測定開始時刻
+int CountFps;					//カウンタ
+float CalcFps;					//計算結果
+int SampleNumFps = GAME_FPS;	//平均を取るサンプル数
+char key[256];
+
+
+
+
+
+VOID MY_FPS_UPDATE(VOID);			//FPS値を計測、更新する
+BOOL LOAD_IMAGE(VOID);		//画像をまとめて読み込む関数
+VOID DELETE_IMAGE(VOID);		//画像をまとめて削除する関数
+
+VOID MY_FPS_WAIT(VOID);				//FPS値を計測し、待つ
+
+BOOL LOAD_MUSIC(VOID);		//音楽をまとめて読み込む関数
+VOID DELETE_MUSIC(VOID);		//音楽をまとめて削除する関数
+VOID MY_ALL_KEYDOWN_UPDATE(VOID);	//キーの入力状態を更新する
+BOOL MY_KEY_DOWN(int);				//キーを押しているか、キーコードで判断する
+BOOL MY_KEY_UP(int);				//キーを押し上げたか、キーコードで判断する
+BOOL MY_KEYDOWN_KEEP(int, int);		//キーを押し続けているか、キーコードで判断する
+
+
+VOID MY_START(VOID);		//スタート画面
+VOID MY_START_PROC(VOID);	//スタート画面の処理
+VOID MY_START_DRAW(VOID);	//スタート画面の描画
+
+VOID MY_SETUMEI(VOID); //説明画面
+VOID MY_START_SETUMEI_DRAW(VOID); //説明画面の描画
+
+VOID MY_PLAY_INIT(VOID);	//プレイ画面初期化
+VOID MY_PLAY(VOID);			//プレイ画面
+VOID MY_PLAY_PROC(VOID);	//プレイ画面の処理
+VOID MY_PLAY_DRAW(VOID);	//プレイ画面の描画
+
+VOID MY_END(VOID);			//エンド画面
+VOID MY_END_PROC(VOID);		//エンド画面の処理
+VOID MY_END_DRAW(VOID);		//エンド画面の描画
+
+
+//イメージ構造体の複製
+IMAGE imageBACK; //タイトル背景
+IMAGE ImageSetumei; //説明画面の画像
+
+//########## プログラムで最初に実行される関数 ##########
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	SetOutApplicationLogValidFlag(FALSE);				//Log.txtを出力しない
+	ChangeWindowMode(TRUE);								//ウィンドウモードに設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//指定の数値でウィンドウを表示する
+	SetWindowStyleMode(0);				//タイトルバーはデフォルトにする
+	SetAlwaysRunFlag(TRUE);								//非アクティブでも実行する
+	
+	SetWindowUserCloseEnableFlag(FALSE);				//閉じるボタンで、勝手にウィンドウが閉じないようにする
+
+	if (DxLib_Init() == -1) { return -1; }	//ＤＸライブラリ初期化処理
+
+	if (LOAD_IMAGE() == FALSE) { return -1; }
+
+	//音楽を読み込む
+	if (LOAD_MUSIC() == FALSE) { return -1; }
+
+	GameScene = GAME_SCENE_START;	//ゲームシーンはスタート画面から
+	SetDrawScreen(DX_SCREEN_BACK);	//Draw系関数は裏画面に描画
+
+	
+
+	//無限ループ
+	while (TRUE)
 	{
-		return -1;			// エラーが起きたら直ちに終了
+		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
+
+		//画面の閉じるボタンを押されたとき
+		if (GetWindowUserCloseFlag(TRUE))
+		{
+			//終了ダイアログを表示
+			int Ret = MessageBox(GetMainWindowHandle(), MSG_CLOSE_CAPTION, MSG_CLOSE_TITLE, MB_YESNO | MB_ICONASTERISK);
+			if (Ret == IDYES) { break; }	//YESなら、ゲームを中断する
+		}
+
+		MY_ALL_KEYDOWN_UPDATE();				//押しているキー状態を取得
+
+		
+
+		MY_FPS_UPDATE();	//FPSの処理[更新]
+
+		//シーンごとに処理を行う
+		switch (GameScene)
+		{
+		case GAME_SCENE_START:
+			MY_START();	//スタート画面
+			break;
+
+		case GAME_SCENE_SETUMEI:
+			MY_SETUMEI(); //説明画面
+			break; 
+
+		case GAME_SCENE_PLAY:
+			MY_PLAY();	//プレイ画面
+			break;
+		case GAME_SCENE_END:
+			MY_END();	//エンド画面
+			break;
+		}
+
+		
+
+		ScreenFlip();		//モニタのリフレッシュレートの速さで裏画面を再描画
+
+		MY_FPS_WAIT();		//FPSの処理[待つ]
 	}
 
-	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+	//画像ハンドルを破棄
+	DELETE_IMAGE();
 
-	WaitKey();				// キー入力待ち
+	//音楽ハンドルを破棄
+	DELETE_MUSIC();
 
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
+	DxLib_End();	//ＤＸライブラリ使用の終了処理
 
-	return 0;				// ソフトの終了 
+	return 0;
+}
+
+
+//########## FPS値を計測、更新する関数 ##########
+VOID MY_FPS_UPDATE(VOID)
+{
+	if (CountFps == 0) //1フレーム目なら時刻を記憶
+	{
+		StartTimeFps = GetNowCount();
+	}
+
+	if (CountFps == SampleNumFps) //60フレーム目なら平均を計算
+	{
+		int now = GetNowCount();
+		//現在の時間から、0フレーム目の時間を引き、FPSの数値で割る＝1FPS辺りの平均時間が計算される
+		CalcFps = 1000.f / ((now - StartTimeFps) / (float)SampleNumFps);
+		CountFps = 0;
+		StartTimeFps = now;
+	}
+	CountFps++;
+	return;
+}
+
+//########## FPS値を計測し、待つ関数 ##########
+VOID MY_FPS_WAIT(VOID)
+{
+	int resultTime = GetNowCount() - StartTimeFps;					//かかった時間
+	int waitTime = CountFps * 1000 / GAME_FPS - resultTime;	//待つべき時間
+
+	if (waitTime > 0)		//指定のFPS値よりも早い場合
+	{
+		WaitTimer(waitTime);	//待つ
+	}
+	return;
+}
+
+//########## キーの入力状態を更新する関数 ##########
+VOID MY_ALL_KEYDOWN_UPDATE(VOID)
+{
+
+	//一時的に、現在のキーの入力状態を格納する
+	char TempKey[KEY_CODE_KIND];
+
+	//直前のキー入力をとっておく
+	for (int i = 0; i < KEY_CODE_KIND; i++)
+	{
+		OldAllKeyState[i] = AllKeyState[i];
+	}
+
+	GetHitKeyStateAll(TempKey); // 全てのキーの入力状態を得る
+
+	for (int i = 0; i < KEY_CODE_KIND; i++)
+	{
+		if (TempKey[i] != 0)	//押されているキーのキーコードを押しているとき
+		{
+			AllKeyState[i]++;	//押されている
+		}
+		else
+		{
+			AllKeyState[i] = 0;	//押されていない
+		}
+	}
+	return;
+}
+
+//キーを押しているか、キーコードで判断する
+//引　数：int：キーコード：KEY_INPUT_???
+BOOL MY_KEY_DOWN(int KEY_INPUT_)
+{
+	//キーコードのキーを押している時
+	if (AllKeyState[KEY_INPUT_] != 0)
+	{
+		return TRUE;	//キーを押している
+	}
+	else
+	{
+		return FALSE;	//キーを押していない
+	}
+}
+
+//スタート画面
+VOID MY_START(VOID)
+{
+	MY_START_PROC();	//スタート画面の処理
+	MY_START_DRAW();	//スタート画面の描画
+	
+
+	
+	DrawString(0, 0, "スタート画面(エンターキーを押して下さい)", GetColor(255, 255, 255));
+	return;
+}
+
+//スタート画面の処理
+VOID MY_START_PROC(VOID)
+{
+	
+	//エンターキーを押したら、プレイシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_RETURN) == TRUE)
+	{
+
+		//ゲームのシーンをプレイ画面にする
+		GameScene = GAME_SCENE_SETUMEI;
+
+		return;
+	}
+
+	return;
+}
+
+//スタート画面の描画
+VOID MY_START_DRAW(VOID)
+{
+	DrawGraph(imageBACK.x, imageBACK.y, imageBACK.handle, TRUE);
+	/*DrawExtendGraph(imageBACK.x, imageBACK.y, 1136, 640, imageBACK.handle, TRUE);*/
+	return;
+}
+
+VOID MY_SETUMEI(VOID)
+{
+	MY_START_SETUMEI_DRAW();//説明画面の描画
+}
+//説明画面の描画
+VOID MY_START_SETUMEI_DRAW(VOID)
+{
+	DrawGraph(ImageSetumei.x, ImageSetumei.y, ImageSetumei.handle, TRUE);
+	DrawString(0, 0, "スタート画面(Hキーを押して下さい)", GetColor(255, 255, 255));
+
+	if (MY_KEY_DOWN(KEY_INPUT_H) == TRUE)
+	{
+		GameScene = GAME_SCENE_PLAY;
+	}
+
+	return;
+}
+
+//プレイ画面
+VOID MY_PLAY(VOID)
+{
+	MY_PLAY_PROC();	//プレイ画面の処理
+	MY_PLAY_DRAW();	//プレイ画面の描画
+
+	DrawString(0, 0, "プレイ画面(スペースキーを押して下さい)", GetColor(255, 255, 255));
+	return;
+}
+
+//プレイ画面の処理
+VOID MY_PLAY_PROC(VOID)
+{
+	//スペースキーを押したら、エンドシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_SPACE) == TRUE)
+	{
+		//ゲームのシーンをエンド画面にする
+		GameScene = GAME_SCENE_END;
+
+		return;
+	}
+
+	
+	
+
+	return;
+}
+
+//プレイ画面の描画
+VOID MY_PLAY_DRAW(VOID)
+{
+
+	
+	for (int tate = 0; tate < MAP_HEIGHT_MAX; tate++)
+	{
+		for (int yoko = 0; yoko < MAP_WIDTH_MAX; yoko++)
+		{
+			//マップを描画
+			DrawGraph(
+				map[tate][yoko].x,
+				map[tate][yoko].y,
+				mapChip.handle[map[tate][yoko].kind],
+				TRUE);
+		}
+	}
+
+	for (int tate = 0; tate < MAP_HEIGHT_MAX; tate++)
+	{
+		for (int yoko = 0; yoko < MAP_WIDTH_MAX; yoko++)
+		{
+			//マップを描画
+			DrawGraph(
+				map[tate][yoko].x,
+				map[tate][yoko].y,
+				mapChip_Nowalk.handle[map[tate][yoko].kind],
+				TRUE);
+		}
+	}
+
+	return;
+}
+
+//エンド画面
+VOID MY_END(VOID)
+{
+	MY_END_PROC();	//エンド画面の処理
+	MY_END_DRAW();	//エンド画面の描画
+
+
+	return;
+}
+
+//エンド画面の処理
+VOID MY_END_PROC(VOID)
+{
+	//エスケープキーを押したら、スタートシーンへ移動する
+	if (MY_KEY_DOWN(KEY_INPUT_ESCAPE) == TRUE)
+	{
+		GameScene = GAME_SCENE_START;
+	}
+
+	DrawString(0, 0, "エンド画面(エスケープキーを押して下さい)", GetColor(255, 255, 255));
+	return;
+}
+
+//エンド画面の描画
+VOID MY_END_DRAW(VOID)
+{
+
+	return;
+}
+
+
+//画像をまとめて読み込む関数
+BOOL LOAD_IMAGE(VOID)
+{
+	strcpy_s(imageBACK.path, TITLE_BACK_PATH);	//タイトル背景画像読込
+	imageBACK.handle = LoadGraph(imageBACK.path);			//読み込み
+	if (imageBACK.handle == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), TITLE_BACK_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+	GetGraphSize(imageBACK.handle, &imageBACK.width, &imageBACK.height);	//画像の幅と高さを取得
+	imageBACK.x = GAME_WIDTH / 2 - imageBACK.width / 2;		//左右中央揃え
+	imageBACK.y = GAME_HEIGHT / 2 - imageBACK.height / 2;		//上下中央揃え
+
+	strcpy_s(ImageSetumei.path, IMAGE_SETUMEI_PATH);	//説明画面背景画像読込
+	ImageSetumei.handle = LoadGraph(ImageSetumei.path);			//読み込み
+	if (ImageSetumei.handle == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), IMAGE_SETUMEI_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+	GetGraphSize(ImageSetumei.handle, &ImageSetumei.width, &ImageSetumei.height);	//画像の幅と高さを取得
+	ImageSetumei.x = GAME_WIDTH / 2 - ImageSetumei.width / 2;		//左右中央揃え
+	ImageSetumei.y = GAME_HEIGHT / 2 - ImageSetumei.height / 2;		//上下中央揃え
+
+	int mapRes = LoadDivGraph(
+		GAME_MAP_PATH,										//赤弾のパス
+		MAP_DIV_NUM, MAP_DIV_TATE, MAP_DIV_YOKO,			//赤弾を分割する数
+		MAP_DIV_WIDTH, MAP_DIV_HEIGHT,						//画像を分割するの幅と高さ
+		&mapChip.handle[0]);								//分割した画像が入るハンドル
+
+	if (mapRes == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), GAME_MAP_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+
+	//幅と高さを取得
+	GetGraphSize(mapChip.handle[0], &mapChip.width, &mapChip.height);
+
+	for (int tate = 0; tate < MAP_HEIGHT_MAX; tate++)
+	{
+		for (int yoko = 0; yoko < MAP_WIDTH_MAX; yoko++)
+		{
+			//マップデータ初期化用に情報をコピー
+			mapDataInit[tate][yoko] = MapData[tate][yoko];
+
+			//マップの種類をコピー
+			map[tate][yoko].kind = MapData[tate][yoko];
+
+			//マップの幅と高さをコピー
+			map[tate][yoko].width = mapChip.width;
+			map[tate][yoko].height = mapChip.height;
+
+			//マップの座標を設定
+			map[tate][yoko].x = yoko * map[tate][yoko].width;
+			map[tate][yoko].y = tate * map[tate][yoko].height;
+		}
+	}
+
+	int mapRes2 =LoadDivGraph(
+		GAME_MAP_PATH,										//赤弾のパス
+		MAP_DIV_NUM, MAP_DIV_TATE, MAP_DIV_YOKO,			//赤弾を分割する数
+		MAP_DIV_WIDTH, MAP_DIV_HEIGHT,						//画像を分割するの幅と高さ
+		&mapChip_Nowalk.handle[0]);								//分割した画像が入るハンドル
+
+	if (mapRes2 == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), GAME_MAP_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+
+	GetGraphSize(mapChip_Nowalk.handle[0], &mapChip_Nowalk.width, &mapChip_Nowalk.height);
+	for (int tate = 0; tate < MAP_HEIGHT_MAX; tate++)
+	{
+		for (int yoko = 0; yoko < MAP_WIDTH_MAX; yoko++)
+		{
+			//マップデータ初期化用に情報をコピー
+			mapDataInit_Nowalk[tate][yoko] = MapData_NoWalk[tate][yoko];
+
+			//マップの種類をコピー
+			map[tate][yoko].kind = MapData_NoWalk[tate][yoko];
+
+			//マップの幅と高さをコピー
+			map[tate][yoko].width = mapChip.width;
+			map[tate][yoko].height = mapChip.height;
+
+			//マップの座標を設定
+			map[tate][yoko].x = yoko * map[tate][yoko].width;
+			map[tate][yoko].y = tate * map[tate][yoko].height;
+		}
+	}
+
+
+	int mapRes = LoadDivGraph(
+		GAME_MAP_PATH,										//赤弾のパス
+		MAP_DIV_NUM, MAP_DIV_TATE, MAP_DIV_YOKO,			//赤弾を分割する数
+		MAP_DIV_WIDTH, MAP_DIV_HEIGHT,						//画像を分割するの幅と高さ
+		&mapChip_Nowalk.handle[0]);								//分割した画像が入るハンドル
+
+	if (mapRes2 == -1)
+	{
+		//エラーメッセージ表示
+		MessageBox(GetMainWindowHandle(), GAME_MAP_PATH, IMAGE_LOAD_ERR_TITLE, MB_OK);
+		return FALSE;
+	}
+
+	GetGraphSize(mapChip_Nowalk.handle[0], &mapChip_Nowalk.width, &mapChip_Nowalk.height);
+	for (int tate = 0; tate < MAP_HEIGHT_MAX; tate++)
+	{
+		for (int yoko = 0; yoko < MAP_WIDTH_MAX; yoko++)
+		{
+			//マップデータ初期化用に情報をコピー
+			mapDataInit_Nowalk[tate][yoko] = MapData_NoWalk[tate][yoko];
+
+			//マップの種類をコピー
+			map[tate][yoko].kind = MapData_NoWalk[tate][yoko];
+
+			//マップの幅と高さをコピー
+			map[tate][yoko].width = mapChip.width;
+			map[tate][yoko].height = mapChip.height;
+
+			//マップの座標を設定
+			map[tate][yoko].x = yoko * map[tate][yoko].width;
+			map[tate][yoko].y = tate * map[tate][yoko].height;
+		}
+	}
+
+	return TRUE;
+}
+
+//画像をまとめて削除する関数
+VOID DELETE_IMAGE(VOID)
+{
+	DeleteGraph(imageBACK.handle);
+	DeleteGraph(ImageSetumei.handle);
+	for (int i_num = 0; i_num < MAP_DIV_NUM; i_num++) { DeleteGraph(mapChip.handle[i_num]); }
+	for (int i_num = 0; i_num < MAP_DIV_NUM; i_num++) { DeleteGraph(mapChip_Nowalk.handle[i_num]); }
+	return;
+}
+
+//音楽をまとめて読み込む関数
+BOOL LOAD_MUSIC(VOID)
+{
+
+	return TRUE;
+}
+
+//音楽をまとめて削除する関数
+VOID DELETE_MUSIC(VOID)
+{
+
+	return;
 }
